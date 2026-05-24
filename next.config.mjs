16b13898e7869bf8c9ef6688ebe49e5f359d66dd
Jan Cornelius Miguel Alcaide <your_github_email@example.com>
2026-05-25 05:54:09 +0800

    Disable TS and ESLint checks

--- next.config.mjs
+++ next.config.mjs
@@ -6,5 +6,14 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: __dirname,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
+
 export default nextConfig;
